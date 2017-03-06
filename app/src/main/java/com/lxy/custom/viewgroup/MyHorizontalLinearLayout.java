973ffae05d4bce54a58761477f658dff42bee0c3
imutlxy <1162981617@qq.com>
2017-03-06 15:44:26 +0800

    子View支持margin

--- app/src/main/java/com/lxy/custom/viewgroup/MyHorizontalLinearLayout.java
+++ app/src/main/java/com/lxy/custom/viewgroup/MyHorizontalLinearLayout.java
@@ -14,7 +14,7 @@ import android.view.ViewGroup;
     /*
     * 1. 父控件本身支持自动换行(done)
     * 2. 子控件支持 gravity
-    * 3. 子控件支持 margin
+    * 3. 子控件支持 margin (done)
     * 4. 子控件支持 padding
     * 5. 子控件支持 visibility (done)
     * 6. 父控件本身支持 wrap_content (done)
@@ -91,102 +91,110 @@ public class MyHorizontalLinearLayout extends ViewGroup {
 
         int count = getChildCount();
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; ++i) {
             View childView = getChildAt(i);
 
-            if (childView.getVisibility() != View.GONE) {
+            //遗留问题:如果把这段代码放开,部分子View显示不正常,具体原因不清楚
+//            if (childView.getVisibility() == View.GONE) {
+//                continue;
+//            }
 
-                //依次测量各个子View
-                measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+            //依次测量各个子View
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
 
-                // 得到child的lp
-                MarginLayoutParams lp = (MarginLayoutParams) childView.getLayoutParams();
-                // 当前子控件实际占据的宽度
-                int childWidth = childView.getMeasuredWidth() + lp.leftMargin
-                        + lp.rightMargin;
-                // 当前子控件实际占据的高度
-                int childHeight = childView.getMeasuredHeight() + lp.topMargin
-                        + lp.bottomMargin;
+            // 得到child的lp
+            MarginLayoutParams lp = (MarginLayoutParams) childView.getLayoutParams();
+            // 当前子控件实际占据的宽度
+            int childWidth = childView.getMeasuredWidth() + lp.leftMargin
+                    + lp.rightMargin;
+            // 当前子控件实际占据的高度
+            int childHeight = childView.getMeasuredHeight() + lp.topMargin
+                    + lp.bottomMargin;
 
-                //超出一行,自动换行
-                if (layoutWidth + childWidth > widthSpecSize) {
-                    //前面几行最大的宽度和当前行的宽度哪个大哪个就是新的layoutWidth
-                    layoutWidth = childWidth;
-                    layoutHeight += lastLineHeight;
+            Log.d(TAG, "onLayout()::i=" + i + ",childWidth=" + childWidth + ",childHeight=" + childHeight);
 
-                    lastLineHeight = childHeight;
-                    Log.d(TAG, "i=" + i + ",lastLineHeight=" + lastLineHeight + ",layoutHeight=" + layoutHeight);
-                } else {
-                    layoutWidth += childWidth;
+            //超出一行,自动换行
+            if (layoutWidth + childWidth > widthSpecSize) {
+                //前面几行最大的宽度和当前行的宽度哪个大哪个就是新的layoutWidth
+                layoutWidth = childWidth;
+                layoutHeight += lastLineHeight;
 
-                    Log.d(TAG, "i=" + i + ",lastLineHeight=" + lastLineHeight + ",childMeasuredHeight=" + childHeight);
-
-                    lastLineHeight = Math.max(childHeight, lastLineHeight);
-                }
-
-                if (i == count - 1) {
-                    layoutHeight += lastLineHeight;
-                    Log.d(TAG, "最后一行:layoutHeight=" + layoutHeight);
-                }
-
-                Log.d(TAG, "i=" + i + ",onMeasure()--layoutWidth=" + layoutWidth + ",layoutHeight=" + layoutHeight + ",childView.measuredWidth=" + childView.getMeasuredWidth() + ",childView.measuredHeight=" + childView.getMeasuredHeight());
+                lastLineHeight = childHeight;
+            } else {
+                layoutWidth += childWidth;
+                lastLineHeight = Math.max(childHeight, lastLineHeight);
             }
+
+            if (i == count - 1) {
+                layoutHeight += lastLineHeight;
+            }
+
+            Log.d(TAG, "i=" + i + ",onMeasure()--layoutWidth=" + layoutWidth + ",layoutHeight=" + layoutHeight + ",childView.measuredWidth=" + childView.getMeasuredWidth() + ",childView.measuredHeight=" + childView.getMeasuredHeight());
         }
+
+        Log.d(TAG, "onMeasure()::parentWidth=" + layoutWidth + ",parentHeight=" + layoutHeight);
 
         setMeasuredDimension(widthSpecMode == MeasureSpec.EXACTLY ? widthSpecSize : layoutWidth, heightSpecMode == MeasureSpec.EXACTLY ? heightSpecSize : layoutHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-//        Log.d(TAG, "left=" + left + ",top=" + top + ",right=" + right + ",bottom=" + bottom);
-
         int count = getChildCount();
 
         int layoutWidth = 0;//目前layout已经占据的宽度
         int layoutHeight = 0;//目前layout已经占据的高度
 
         int parentWidth = getWidth();
+        int parentHeight = getHeight();
 
-//        Log.d(TAG, "onLayout()::parentWidth=" + parentWidth + ",parentHeight=" + parentHeight + ",getMeasuredWidth=" + getMeasuredWidth() + ",getMeasuredHeight=" + getMeasuredHeight());
+        Log.d(TAG, "onLayout()::parentWidth=" + parentWidth + ",parentHeight=" + parentHeight);
+
 
         int lastLayoutHeight = 0;//上一行子View占据的高度,用于超出一行时重新计算View的高度
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; ++i) {
             View childView = getChildAt(i);
-            int measuredWidth = childView.getMeasuredWidth();
-            int measuredHeight = childView.getMeasuredHeight();
 
-            //超出一行,自动换行
-            if (layoutWidth + measuredWidth > parentWidth) {
-//                Log.d(TAG, "i=" + i + ",parentWidth=" + parentWidth + ",layoutWidth + measuredWidth=" + (layoutWidth + measuredWidth));
-                //如果超出一行,当前layout的高度=已经占据的高度+上一行View的高度
-                layoutHeight += lastLayoutHeight;
+            if (childView.getVisibility() != View.GONE) {
 
-                left = 0;
-                right = left + measuredWidth;
-                top = layoutHeight;
-                bottom = top + measuredHeight;
+                // 得到child的lp
+                MarginLayoutParams lp = (MarginLayoutParams) childView.getLayoutParams();
 
-                layoutWidth = measuredWidth;
-                lastLayoutHeight = measuredHeight;
 
-//                Log.d(TAG, "i=" + i + ",lastLayoutHeight=" + lastLayoutHeight + ",layoutHeight=" + layoutHeight);
-//                Log.d(TAG, "i=" + i + ",measuredHeight=" + measuredHeight + ",超出一行--left=" + left + ",top=" + top + ",right=" + right + ",bottom=" + bottom);
+                int childWidth = childView.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+                int childHeight = childView.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+                //超出一行,自动换行
+                if (layoutWidth + childWidth > parentWidth) {
+                    //如果超出一行,当前layout的高度=已经占据的高度+上一行View的高度
+                    layoutHeight += lastLayoutHeight;
+
+                    top = layoutHeight + lp.topMargin;
+                    left = 0;
+                    bottom = top + childView.getMeasuredHeight();
+                    right = left + childView.getMeasuredWidth();
+
+                    layoutWidth = childWidth;
+                    lastLayoutHeight = childHeight;
+                } else {
+                    //没有超出一行
+
+                    top = layoutHeight + lp.topMargin;
+                    left = layoutWidth + lp.leftMargin;
+                    bottom = top + childView.getMeasuredHeight();
+                    right = left + childView.getMeasuredWidth();
+
+                    layoutWidth += childWidth;
+
+                    lastLayoutHeight = Math.max(lastLayoutHeight, childHeight);
+                }
+
+                Log.d(TAG, "i=" + i + ",left=" + left + ",right=" + right + ",top=" + top + ",bottom=" + bottom);
+
+                childView.layout(left, top, right, bottom);
             } else {
-                //没有超出一行
-                left = layoutWidth;
-                right = left + measuredWidth;
-                top = layoutHeight;
-                bottom = top + measuredHeight;
-
-                layoutWidth += measuredWidth;
-
-                lastLayoutHeight = Math.max(lastLayoutHeight, measuredHeight);
-
-//                Log.d(TAG, "i=" + i + ",lastLayoutHeight=" + lastLayoutHeight + ",layoutHeight=" + layoutHeight);
-//                Log.d(TAG, "i=" + i + ",measuredHeight=" + measuredHeight + ",没有超出一行--left=" + left + ",top=" + top + ",right=" + right + ",bottom=" + bottom);
+                Log.d(TAG, "i=" + i + ",childView is gone");
             }
-            childView.layout(left, top, right, bottom);
         }
     }
 
